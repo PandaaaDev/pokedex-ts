cc0d5823a934f91c973f48111014c85c0b3f4e47
Eryk Juszczak <86883286+PandaaaDev@users.noreply.github.com>
2023-06-27 09:35:22 +0200

    Changes to styles and mapin of types

--- src/components/Molecules/Pokemon.tsx
+++ src/components/Molecules/Pokemon.tsx
@@ -2,6 +2,10 @@ import { PokemonType } from '@/types/main';
 import useFetch from '@/hooks/useFetch';
 import styled from 'styled-components';
 const StyledPokemon = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
 	width: 100%;
 	height: 350px;
 	max-width: 350px;
@@ -10,12 +14,13 @@ const StyledPokemon = styled.div`
 	border: 1px solid red;
 	border-radius: 25px;
 	img {
-		width: 50%;
-		height: 50%;
+		width: 100px;
+		height: 100px;
 	}
 `;
 const Pokemon: React.FC<PokemonType> = ({ name, url }) => {
 	const { data, loading, error } = useFetch(url);
+	console.log(data);
 	if (error) {
 		<div>An error occurred sorry!</div>;
 	}
@@ -32,6 +37,14 @@ const Pokemon: React.FC<PokemonType> = ({ name, url }) => {
 								src={data?.sprites?.front_default}
 								alt={`Image of ${name}`}
 							></img>
+						</div>
+						<div>
+							<p>Type:</p>
+							{data.types.map(
+								(e: { slot: number; type: { name: string; url: string } }) => {
+									return <p>{e.type.name}</p>;
+								}
+							)}
 						</div>
 						<div>{name}</div>
 					</StyledPokemon>
